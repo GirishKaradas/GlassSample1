6b24c81655b486ad22b83b4bdeb6be3ef805d4c5
GirishKaradas <36401945+GirishKaradas@users.noreply.github.com>
2021-02-20 14:03:46 +0530

    Update MainLayoutFragment.java

--- GlassSample/src/main/java/com/genius/glasssample/fragments/MainLayoutFragment.java
+++ GlassSample/src/main/java/com/genius/glasssample/fragments/MainLayoutFragment.java
@@ -18,13 +18,15 @@ package com.genius.glasssample.fragments;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.genius.glasssample.R;
 
