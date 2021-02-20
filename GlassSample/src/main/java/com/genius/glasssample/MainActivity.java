199bd097751695fa9f115444ef0555911b1bf263
Girish Karadas <girishkaradas@gmail.com>
2021-02-20 13:07:04 +0530

    Initial commit

---
+++ GlassSample/src/main/java/com/genius/glasssample/MainActivity.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.genius.glasssample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.glass.ui.GlassGestureDetector;
+import com.genius.glasssample.fragments.BaseFragment;
+import com.genius.glasssample.fragments.ColumnLayoutFragment;
+import com.genius.glasssample.fragments.MainLayoutFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main activity of the application. It provides viewPager to move between fragments.
+ */
+public class MainActivity extends BaseActivity {
+
+    private List<BaseFragment> fragments = new ArrayList<>();
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_pager_layout);
+
+        final ScreenSlidePagerAdapter screenSlidePagerAdapter = new ScreenSlidePagerAdapter(
+            getSupportFragmentManager());
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(screenSlidePagerAdapter);
+
+        fragments.add(MainLayoutFragment
+            .newInstance("Start Unity", getString(R.string.footnote_sample),
+                getString(R.string.timestamp_sample), null));
+   /*     fragments.add(MainLayoutFragment
+            .newInstance(getString(R.string.different_options), getString(R.string.empty_string),
+                getString(R.string.empty_string), null));
+        fragments.add(ColumnLayoutFragment
+            .newInstance(R.drawable.ic_style, getString(R.string.columns_sample),
+                getString(R.string.footnote_sample), getString(R.string.timestamp_sample)));
+        fragments.add(MainLayoutFragment
+            .newInstance(getString(R.string.like_this_sample), getString(R.string.empty_string),
+                getString(R.string.empty_string), null));
+
+
+    */
+        screenSlidePagerAdapter.notifyDataSetChanged();
+
+        final TabLayout tabLayout = findViewById(R.id.page_indicator);
+        tabLayout.setupWithViewPager(viewPager, true);
+    }
+
+    @Override
+    public boolean onGesture(GlassGestureDetector.Gesture gesture) {
+        switch (gesture) {
+            case TAP:
+                startActivity(new Intent(MainActivity.this, UnityPlayerActivity.class));
+                Toast.makeText(this, "Opening Unity", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onGesture(gesture);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public BaseFragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+}
